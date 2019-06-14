6cac9c216730a04dfb9f1f6e37a3107501e1a997
Roni <roni.colling1@gmail.com>
2019-06-14 19:15:40 +0100

    add login stuff to navbar

--- server.js
+++ server.js
@@ -47,8 +47,8 @@ db.once('open', function callback() {
 //   mongoMessage = messageDoc.message;
 // });
 
-app.get('/partials/:partialPath', function(req, res){
-    res.render('partials/' + req.params.partialPath)
+app.get('/partials/*', function(req, res){
+    res.render('../../public/app/' + req.params[0])
 })
 
 app.get('*', function(req, res) {
