242d317347d791ae4448fbb7cba7f5c46312b629
Roni <roni.colling1@gmail.com>
2019-06-15 15:55:57 +0100

    remove config from gitignore

---
+++ config.js
@@ -0,0 +1,3 @@
+module.exports = {
+    dbUrl: 'mongodb://roni:multivision1@ds135427.mlab.com:35427/multivision'
+}
---
+++ server/config/config.js
@@ -0,0 +1,14 @@
+const path = require('path');
+const rootPath = path.normalize(__dirname + '/../../')
+module.exports = {
+    development: {
+        rootPath: rootPath,
+        db: 'mongodb://localhost/multivision',
+        port: process.env.PORT || 3030
+    },
+    production: {
+        rootPath: rootPath,
+        db: 'mongodb://roni:multivision1@ds135427.mlab.com:35427/multivision',
+        port: process.env.PORT || 80
+    }
+}
